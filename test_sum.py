d9894acc4742e4a8b65678f7a1c0b30964b5df9e
Daniel Ramos <danielramosm99@gmail.com>
2021-05-24 15:09:41 -0500

    Create the test

--- test_sum.py
+++ test_sum.py
@@ -1,4 +1,14 @@
 # Class tests
 
 import unittest
-import sum.py
+import sum_numbers
+
+class SumTest(unittest.TestCase):
+	def test_sum_numbers(self):
+		self.assertEqual(sum_numbers.sum(2, 10), 12)
+		self.assertEqual(sum_numbers.sum(-2, 10), 8)
+		self.assertEqual(sum_numbers.sum(-2, -10), -12)
+
+
+if __name__ == '__main__':
+	unittest.main()
